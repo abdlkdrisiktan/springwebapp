066afd7f2210de8ef001d9c5b8a76efeaa4185b5
Abdulkadir IŞIKTAN <kadirisiktan@gmail.com>
2018-02-18 23:06:05 +0300

    Lecture 72 - adding bean and creating config file

--- src/main/java/com/guru/springframework/springwebapp/SpringwebappApplication.java
+++ src/main/java/com/guru/springframework/springwebapp/SpringwebappApplication.java
@@ -1,10 +1,24 @@
 package com.guru.springframework.springwebapp;
 
+import com.guru.springframework.springwebapp.config.PropertyConfig;
+import com.guru.springframework.springwebapp.model.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class SpringwebappApplication {
+
+	@Value("${springwebapp.username}")
+	String user;
+
+	@Value("${springwebapp.password}")
+	String password;
+
+	@Value("${springwebapp.dburl}")
+	String url;
+
 
 	//jdbc:h2:mem:testdb JDBC URL
 	public static void main(String[] args) {
---
+++ src/main/java/com/guru/springframework/springwebapp/config/PropertyConfig.java
@@ -0,0 +1,41 @@
+package com.guru.springframework.springwebapp.config;
+
+import com.guru.springframework.springwebapp.model.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+//this annotiation directly looking this
+public class PropertyConfig {
+
+    @Value("${springwebapp.username}")
+    String user;
+
+    @Value("${springwebapp.password}")
+    String password;
+
+    @Value("${springwebapp.dburl}")
+    String url;
+
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+
+
+
+}
